72092000639d55fe9d74e23ae203df0c2c500b1a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-12 16:37:37 +0530

    Update main.tf

--- infra/modules/product_ec2/main.tf
+++ infra/modules/product_ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_servicecatalog_product" "this1" {
+resource "aws_servicecatalog_product" "example" {
   name            = "ec2product"
   owner           = "AWS"
   description     = "A CloudFormation product"
@@ -14,4 +14,18 @@ resource "aws_servicecatalog_product" "this1" {
     type           = "CLOUD_FORMATION_TEMPLATE"  # Change to CloudFormation template type
     template_url   = "https://s3.amazonaws.com/sneha-terraform-bucket-us-west-2/simple_ec2_ebs_appregistry.yaml"
   }
+}
+resource "aws_servicecatalog_product_portfolio_association" "example" {
+  portfolio_id = var.portfolio_id
+  product_id   = aws_servicecatalog_product.example.id
+}
+
+resource "aws_servicecatalog_constraint" "example" {
+  portfolio_id = var.portfolio_id
+  product_id   = aws_servicecatalog_product.example.id
+  type         = "LAUNCH"
+  description  = "Launch constraint for example product"
+  parameters   = jsonencode({
+    RoleArn = var.role_arn
+  })
 }
