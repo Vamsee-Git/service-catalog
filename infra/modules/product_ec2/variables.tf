63496f997c4520c2bba166af1fec18e1030ee463
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-12 16:56:27 +0530

    Create variables.tf

---
+++ infra/modules/product_ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "portfolio_id" {
+  description = "ID of the portfolio where the product will be added"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "IAM role in ARN"
+  type        = string
+}
