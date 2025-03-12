0ac2a90f3d5b5f45e2f9a25e535bd1fe9f801f32
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-12 13:18:15 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,12 @@
 variable "portfolio_name" {
   description = "The name of the AWS Service Catalog portfolio"
   type        = string
+  default     = terraform-portfolio
 }
 
 variable "product_name" {
   description = "The name of the AWS Service Catalog product"
-  type        = string
+  type        = s3-product
 }
 
 # You can define other variables in a similar fashion if needed.
