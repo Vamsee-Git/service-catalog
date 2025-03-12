69dfebc4ca2009420e426efb643302e8533290dc
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-12 08:26:32 +0530

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "portfolio_name" {
+  description = "The name of the AWS Service Catalog portfolio"
+  type        = string
+}
+
+variable "product_name" {
+  description = "The name of the AWS Service Catalog product"
+  type        = string
+}
+
+# You can define other variables in a similar fashion if needed.
