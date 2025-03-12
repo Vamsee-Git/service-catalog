4b5fd60e7c8591774497f438c808dc6cea818afa
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-12 14:06:19 +0530

    Update variables.tf

--- infra/modules/product/variables.tf
+++ infra/modules/product/variables.tf
@@ -47,3 +47,8 @@ variable "portfolio_id" {
   description = "ID of the portfolio where the product will be added"
   type        = string
 }
+
+variable "role_arn" {
+  description = "IAM role in ARN"
+  type        = string
+}
